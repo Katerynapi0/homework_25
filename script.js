89444db437a2ba2fca08bf4b9d42c0884aea9d75
Katerynapi0 <d.olga0502@gmail.com>
2024-02-16 14:10:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict'
+/*Створити клас SuperMath.
+
+Додати до екземпляра метод - check(obj), параметр obj якого має властивості X, Y, znak. 
+Метод повинен підтвердити у користувача, чи хоче він зробити дію znak c Х і У. 
+Якщо хоче, зробити математичну дію znak (яка описана в прототипі), інакше - запитати введення нових даних через метод input() класу SuperMath. 
+
+Приклад об'єкта: `obj = {X:12, Y:3, znak: “/”}`, можливі варіанти znak => `+ - / * %`.
+
+При введенні znak потрібно перевірити коректність введення на можливі математичні дії.
+
+p = new SuperMath();
+p.check(obj); // --> no p.input() -> 3 prompt -> рахує*/
+
+class SuperMath{
+    check(obj){
+        const result = this.calculate(obj.X, obj.Y, obj.znak);
+        if(isNaN(result) || result === undefined){
+            alert(`Помилка. Ви ввели некоректні дані.`)
+        }else{
+            alert(`Ваш результат: ${result}`);
+        }
+        this.input();
+    }
+
+    calculate(X, Y, znak){
+        switch(znak){
+            case '+':
+            return X + Y;
+
+            case '-':
+            return X - Y;
+
+            case '/':
+            return X / Y;
+
+            case '*':
+            return X * Y;
+
+            case '%':
+            return X % Y;
+
+            default:
+            return `Некоректно введена математична дія.`;
+        }
+    }
+
+    input(){
+        const newX = parseFloat(prompt(`Введіть перше числове значення:`));
+        const newY = parseFloat(prompt(`Введіть друге числове значення:`));
+        const newZnak = prompt(`Введіть одну з математичних дії, яку хочете виконати. 
+        Можливі варіанти:
+        1. +
+        2. -
+        3. /
+        4. *
+        5. %`);
+
+        if(isNaN(newX) || isNaN(newY)){
+            alert(`Введіть число, будь ласка!`);
+        }
+
+        const obj = {X: newX, Y: newY, znak: newZnak};
+        this.check(obj);
+    }
+}
+
+const p = new SuperMath;
+p.input();
